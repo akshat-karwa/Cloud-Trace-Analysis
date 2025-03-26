62e22f6e3b1424e7c4d6a5f646cae7c819499096
Mehul <mehulrastogi2002@gmail.com>
2025-03-26 15:41:59 -0400

    azure-data-analysis

--- azure_analysis.py
+++ azure_analysis.py
@@ -1,10 +1,19 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 def read_csv():
-    vm_table_data = pd.read_csv("vm_table_data_azure.csv", nrows = 10000000)
+    vm_table_data = pd.read_csv("vm_table_data_azure.csv")
     return vm_table_data
 
-def analysis():
+def graph_utilization_rates(metric):
     vm_table_data = read_csv()
+    plt.figure(figsize = (12, 6))
+    plt.hist(vm_table_data[metric], bins = 20, rwidth = 0.95, color = "green")
+    plt.title("Distribution of " + str(metric) + " CPU values, for Azure")
+    plt.ylabel("Number of VMs")
+    plt.xlabel("Percentage (%) Utilization")
+    plt.show()
 
-analysis()
+graph_utilization_rates("max_cpu")
+graph_utilization_rates("avg_cpu")
+graph_utilization_rates("p95_max_cpu")
