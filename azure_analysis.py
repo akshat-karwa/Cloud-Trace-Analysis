bd5e7b95c62708051271b4191a77804021930646
Mehul <mehulrastogi2002@gmail.com>
2025-04-15 16:46:12 -0400

    Azure data analysis

--- azure_analysis.py
+++ azure_analysis.py
@@ -5,15 +5,25 @@ def read_csv():
     vm_table_data = pd.read_csv("vm_table_data_azure.csv")
     return vm_table_data
 
-def graph_utilization_rates(metric):
-    vm_table_data = read_csv()
-    plt.figure(figsize = (10, 6)) # @Akshat, change as neeeded, we can finalize upon one
-    plt.hist(vm_table_data[metric], bins = 20, rwidth = 0.95, color = "green")
-    plt.title("Distribution of " + str(metric) + " CPU values, for Azure")
-    plt.ylabel("Number of VMs")
-    plt.xlabel("Percentage (%) Utilization")
+def plot_resource_analysis(dataframe, col_name):
+    plt.figure(figsize = (10, 6))
+    plt.hist(dataframe[col_name], bins = 40, rwidth = 0.85, color = "dodgerblue")
+    heading = ""
+    if col_name == "max_cpu":
+        heading = "Max"
+    elif col_name == "avg_cpu":
+        heading = "Average"
+    else:
+        heading = "P95 Max"
+    plt.title("Distribution of " + str(heading) + " CPU Utilization Percentage Values for Azure")
+    plt.xlabel("CPU Utilization (%)")
+    plt.ylabel("Number of Virtual Machines")
+    plt.grid(True)
+    plt.savefig(f'azure_{col_name}_distribution.png', dpi=300, bbox_inches='tight')
     plt.show()
 
-graph_utilization_rates("max_cpu")
-graph_utilization_rates("avg_cpu")
-graph_utilization_rates("p95_max_cpu")
+vm_table_data = read_csv()
+print(vm_table_data["avg_cpu"].mean())
+#plot_resource_analysis(vm_table_data, "max_cpu")
+#plot_resource_analysis(vm_table_data, "avg_cpu")
+#plot_resource_analysis(vm_table_data, "p95_max_cpu")
